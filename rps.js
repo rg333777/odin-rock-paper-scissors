21897518cf25a2b6503262501090ef73ba94fa36
rg333777 <143126002+rg333777@users.noreply.github.com>
2024-10-07 18:00:38 +0100

    created logic for single round of play

--- rps.js
+++ rps.js
@@ -13,8 +13,47 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    const choice = prompt('rock, paper or scissors?')
+    const choice = prompt('rock, paper or scissors?').toLowerCase()
     return choice
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice()
+    computerChoice = getComputerChoice()
+    console.log(humanChoice)
+    console.log(computerChoice)
+    if (humanChoice === computerChoice) {
+        return 'this is a tie!'
+    }
+    if (humanChoice === 'rock') {
+        if (computerChoice == 'scissors') {
+            humanScore++
+            return 'you win!'
+        } else {
+            computerScore++
+            return 'you lose!'
+        }
+    }
+    if (humanChoice === 'scissors') {
+        if (computerChoice === 'paper') {
+            humanScore++
+            return 'you win!'
+        } else {
+            computerScore++
+            return 'you lose!'
+        }
+    }
+    if (humanChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            humanScore++
+            return 'you win!'
+        } else {
+            computerScore++
+            return 'you lose'
+        }
+    }   
+}
+
+console.log(playRound())
+console.log(humanScore)
+console.log(computerScore)
