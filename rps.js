5162d2cceb73364e147feb5aa623b30bb4357fcb
rg333777 <143126002+rg333777@users.noreply.github.com>
2024-10-07 17:32:49 +0100

    set up logic for computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+function getComputerChoice() {
+    const choice =  Math.random()
+    if (choice < 0.33) {
+        return 'rock'
+    } else if (choice > 0.66) {
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
+}
+
+console.log(getComputerChoice())
